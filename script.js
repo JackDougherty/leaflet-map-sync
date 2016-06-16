d70cc538ec26350428df914a04509cd93823fee1
Jack Dougherty <jack.dougherty@trincoll.edu>
2016-06-16 11:18:17 -0400

    L.GeoSearch and permalink

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ var esriImagery = L.esri.basemapLayer('Imagery',{
 });
 var esriLabels = L.esri.basemapLayer('ImageryLabels');
 var esriTransportation = L.esri.basemapLayer('ImageryTransportation');
-var esriAll = [esriImagery, esriLabels, esriTransportation]
+var esriPresent = [esriImagery, esriLabels, esriTransportation];
 
 var map1 = L.map('map1', {
-    layers: [magic1934],
+    layers: magic1934,
     center: startCenter,
     zoom: startZoom,
     zoomControl: false,
@@ -43,16 +43,16 @@ var map1 = L.map('map1', {
 });
 
 var map2 = L.map('map2', {
-    layers: esriAll,
+    layers: esriPresent,
     center: startCenter,
     zoom: startZoom,
     zoomControl: false,
     scrollWheelZoom: false
 });
 
-// optional : customize link to view source code; add your own GitHub repository
+// customize link to view source code; add your own GitHub repository
 map1.attributionControl
-.setPrefix('View <a href="http://github.com/jackdougherty/leaflet-map-sync" target="_blank">code on GitHub</a> with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
+.setPrefix('View <a href="http://github.com/jackdougherty/leaflet-map-sync" target="_blank">code on GitHub</a>');
 map2.attributionControl
 .setPrefix('');
 
@@ -60,27 +60,18 @@ map2.attributionControl
 L.control.zoom({position: "topright"}).addTo(map1);
 L.control.zoom({position: "topright"}).addTo(map2);
 
+// Display permalink in URL for users to share
+// FIX to capture layers from map1 and map2 
+map1.addControl(new L.Control.Permalink({text: 'Permalink'}));
+
 L.control.scale().addTo(map2);
 
-// set Esri geocoding provider and place control on map
-// http://esri.github.io/esri-leaflet/api-reference/controls/geosearch.html
-var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider();
-var searchControl = L.esri.Geocoding.geosearch({
-  position: 'topright',
-  providers: [arcgisOnline],
-  expanded: false  // default = false
+new L.Control.GeoSearch({
+				provider: new L.GeoSearch.Provider.Google(),
+        position: 'topleft' // see also style.CSS
 }).addTo(map2);
-// create an empty layer group to store the results and add it to the map
-var results = L.layerGroup().addTo(map2);
-// listen for the results event and add every result to the map
-searchControl.on("results", function(data) {
-  results.clearLayers();
-  for (var i = data.results.length - 1; i >= 0; i--) {
-    results.addLayer(L.marker(data.results[i].latlng));
-  }
-});
 
-// credit: sync code from https://www.mapbox.com/mapbox.js/example/v1.0.0/sync-layer-movement/
+// sync code adapted from https://www.mapbox.com/mapbox.js/example/v1.0.0/sync-layer-movement/
 // when either map finishes moving, trigger an update on the other one
 map1.on('moveend', follow).on('zoomend', follow);
 map2.on('moveend', follow).on('zoomend', follow);
